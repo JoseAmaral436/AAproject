7529415d262056fdc452c055600ee57d98f86180
Jose Francisco Pires Amaral <ist176346@lab10p9.rnl.tecnico.ulisboa.pt>
2016-02-29 17:50:44 +0000

    [ADD] tfind

--- project.c
+++ project.c
@@ -20,6 +20,7 @@ void tsearch(char *s){
     root->numOfOcc = 1;
     root->left = NULL;
     root->right = NULL;
+    printf("%d\n",root->numOfOcc);
   }
   else{
     Node *aux = root;
@@ -65,6 +66,67 @@ void tsearch(char *s){
   }
 }
 
+Node * tfind(char *s){
+  if (root == NULL){
+    return NULL;
+  }
+  else{
+    Node *aux = root;
+    while(1){
+      if((strcmp(aux->string, s)) < 0){
+	if(aux->left == NULL){
+	  return NULL;
+	}
+	else{
+	  aux = aux->left;
+	}
+      }
+      else if((strcmp(aux->string, s)) > 0){
+	if(aux->right == NULL){
+	  return NULL;
+	}
+	else{
+	  aux = aux->right;
+	}
+      }
+      else{
+	printf("%d\n",aux->numOfOcc);
+	return aux;
+      }
+    }
+  }
+}
+
+/*void tdelete(char *s){
+  if (root == NULL){
+    return NULL;
+  }
+  else{
+    Node *aux = root;
+    while(1){
+      if((strcmp(aux->string, s)) < 0){
+	if(aux->left == NULL){
+	  return NULL;
+	}
+	else{
+	  aux = aux->left;
+	}
+      }
+      else if((strcmp(aux->string, s)) > 0){
+	if(aux->right == NULL){
+	  return NULL;
+	}
+	else{
+	  aux = aux->right;
+	}
+      }
+      else{
+	printf("%d\n",aux->numOfOcc);
+	return aux;
+      }
+    }
+  }
+}*/
 
 int main(){
   int numOfLines;
@@ -80,18 +142,24 @@ int main(){
   int i;
   char *line = NULL;
   size_t sizeOfLine;
-  for(i = 0; i < numOfLines; i++){
+  for(i = 1; i <= numOfLines; i++){
     getline(&line, &sizeOfLine, stdin);
-    line[8] = '\0';
-    //printf("hello %d %s size:%d \n", i, line, strlen(line));
+    line[8] = '\0'; //TODO ASK IF ALL STRINGS HAVE SAME LENGTH
     switch(line[0]){
       case 'A':
+	memmove(line, line+1, strlen(line));
+	memmove(line, line+1, strlen(line));
 	tsearch(line);
 	break;
       case 'F':
-	//tfind(line);
+	memmove(line, line+1, strlen(line));
+	memmove(line, line+1, strlen(line));
+	tfind(line);
 	break;
       case 'D':
+	memmove(line, line+1, strlen(line));
+	memmove(line, line+1, strlen(line));
+	printf("D aind n feito\n");
 	//tdelete(line);
 	break;
     }
