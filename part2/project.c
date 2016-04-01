f26f6d7abcb67d3a7dc7f415a7b43016ac30c7a8
Jose Francisco Pires Amaral <jose_amaral436@hotmail.com>
2016-04-01 13:01:19 +0100

    [FIXED] generalized string to array of strings

--- part2/project.c
+++ part2/project.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+
+
+int *ni;
+char **generalizedString;
+
 
 typedef struct node {
 	int Ti; /**< The value of i in Ti */
@@ -23,7 +29,7 @@ void AddLeaf(Point *p, int i, int j){
 	Node *node = malloc(sizeof(Node));
 	node->Ti = i;
 	node->head = j;
-	node->sdep = p->s;
+	node->sdep = ni[i] - j;
 	node->child = NULL;
 	node->brother = NULL;
 	node->slink = NULL;
@@ -40,32 +46,27 @@ void AddLeaf(Point *p, int i, int j){
 		node->hook = &(ancestor->brother);
 	}
 }
-void printString(Node *n, char* string, int ni[]){
-	int index;// = n->Ti * ni[n->Ti];
-	int depth = 0;
-	for (index = 0; index < n->Ti; index++){
-		depth = depth + ni[index] + 1;
-	}
-	depth += n->head;
-	int d = depth;
-	for (; depth < d + n->sdep; depth++){
-		printf("%c", string[depth]);
+void printString(Node *n, char* string[]){
+	int i;
+	int index = n->head + n->sdep + 1;
+	for(i = n->head; i < index; i++){
+		printf("%c", string[n->Ti][i]);
 	}
 	printf("\n");
 	
 }
 
-void printST(Node *root, char* string, int ni[]){
+void printST(Node *root, char* string[]){
 	if (root->child == NULL){
-		printString(root, string, ni);
+		printString(root, string);
 		if (root->brother != NULL){
-			printST(root->brother, string, ni);
+			printST(root->brother, string);
 		}
 	}
 	else{
-		printST(root->child, string, ni);
+		printST(root->child, string);
 		if (root->brother != NULL){
-			printST(root->brother, string, ni);
+			printST(root->brother, string);
 		}
 	}
 	
@@ -75,44 +76,55 @@ void printST(Node *root, char* string, int ni[]){
 void SuffixLink(Point *p){
 	p->a = (p->a)->slink;
 	p->b = (p->b)->slink;
-	
+	p->s = 0;
 }
 
 
 void Descend(Point *p, char s){
-	p->a = *((p->a)->hook);
-	p->b = *((p->b)->hook);
+	p->a = (p->a)->child;
+	p->b = (p->b)->child;
+	p->s += 1;
+}
+
+bool DescendQ(Point *p, char c){
+	printf("c: %c\n", generalizedString[(p->b)->Ti][p->s]);
+	if ((p->a)->child == NULL){
+		return false;
+	}
+	else{
+		//printf("c: %c\n", generalizedString[(p->b)->Ti][p->s]);
+	}
+	return false;
 }
 
 
-Node * buildST(char* string, int ni[],int numberOfStrings, int strLen){
+Node * buildST(char* string[], int ni[],int numberOfStrings){
 	Node *root = malloc(sizeof(Node));
 	Node *sentinel = malloc(sizeof(Node));
 	root->slink = sentinel;
+	root->sdep = -2;
 	root->hook = &(sentinel->child);
 	sentinel->child = root;
+	sentinel->sdep = -1;
 	Point *p = malloc(sizeof(Point));
 	p->a = root;
 	p->b = root;
-	p->s = strLen;
 	int i = 0;
-	int strIndex = 0;
 	while(i < numberOfStrings){
+		p->s = 0;
 		int j = 0;
 		while(j <= ni[i]){
-			//while(!DescendQ(p,string[strIndex+ j])){
+			//DescendQ(p, string[i][j]);
+			//while(!DescendQ(p, string[i][j])){
 				AddLeaf(p, i, j);
-				SuffixLink(p);	
+				SuffixLink(p);
 			//}
-			Descend(p, string[strIndex+ j]);
-			p->s = p->s - 1;
+			Descend(p, string[i][j]);
 			j++;
 		}
-		strIndex = strIndex + ni[i] + 1;
 		i++;
 	}
 	return root;
-
 }
 
 int main() {
@@ -129,40 +141,24 @@ int main() {
 	char *line = NULL;
 	size_t sizeOfLine;
 	char* token;
-	int MAX_SIZE = 20;
-	char* generalizedString = malloc(sizeof(char) * MAX_SIZE);
-	int idx = 0;
-	int strSize[numOfLines];
+	generalizedString = (char**) malloc(sizeof(char*) * numOfLines);
+	ni = malloc(sizeof(int) * numOfLines);
 	for (i = 1; i <= numOfLines; i++) {
 		getline(&line, &sizeOfLine, stdin);
 		int lineSize = strlen(line);
 		if (i != numOfLines) {
-			line[lineSize - 1] = '\0'; //all strings have '\n' in the end beside the last
+			line[lineSize - 1] = '-'; //all strings have '\n' in the end beside the last
 		} else {
-			line[lineSize] = '\0'; // last string doesnt have '\n'
+			line[lineSize] = '-'; // last string doesnt have '\n'
 		}
 		token = strtok(line, " ");
-		strSize[i-1] = atoi(token);
-		if (strlen(generalizedString) + strSize[i-1] >= MAX_SIZE) {
-			generalizedString = realloc(generalizedString,
-					2 * MAX_SIZE + strSize[i-1]);
-			MAX_SIZE *= 2;
-		}
+		ni[i-1] = atoi(token);
 		token = strtok(NULL, " ");
-		int j = 0;
-		int max = idx + strSize[i-1];
-		for (; idx < max; idx++) {
-			generalizedString[idx] = token[j++];
-		}
-		generalizedString[idx] = '-';
-		idx++;
+		generalizedString[i-1] = malloc(sizeof(char) * ni[i-1]);
+		strcpy(generalizedString[i-1], token);
 	}
-	generalizedString[idx] = '\0';
-	//printf("%s\n", generalizedString);
-	/*for (i = 0; i < numOfLines; i++) {
-		printf("%d ", strSize[i]);
-	}*/
-	Node * root = buildST(generalizedString, strSize, numOfLines, idx);
-	printST(root, generalizedString, strSize);
+	
+	Node * root = buildST(generalizedString, ni, numOfLines);
+	printST(root, generalizedString);
 	return 0;
 }
